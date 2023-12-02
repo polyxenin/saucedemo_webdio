b194bd6b49250c793dc19df863addfd5c7f970cb
nakoup <p.nakou@gmail.com>
2023-12-02 09:32:09 +0200

    Add test for image correctness inside product component

--- test/pageobjects/inventory.page.js
+++ test/pageobjects/inventory.page.js
@@ -7,19 +7,21 @@ const Page = require('./page');
  * sub page containing specific selectors and methods for inventory page
  */
 class InventoryPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
+    
     get dropdown() {
         return $('//*[@class="product_sort_container"]');
     }
 
-    get productTitles() {
+    get productsTitles() {
         return $$('//div[@class="inventory_item_name "]');
     }
 
-    get productPrices() {
+    get productsPrices() {
         return $$('//div[@class="inventory_item_price"]');
+    }
+
+    get productsImages() {
+        return $$('//div[@class="inventory_item_img"]/a/img');
     }
 
     async select(value) {
--- test/specs/inventory.js
+++ test/specs/inventory.js
@@ -1,5 +1,8 @@
+const items = require('../../resources/data/inventory_items.json');
+
 const _ = require('lodash');
 const expectChai = require('chai').expect;
+
 const LoginPage = require('../pageobjects/login.page');
 const InventoryPage = require('../pageobjects/inventory.page');
 
@@ -17,47 +20,32 @@ describe('Inventory Page', () => {
         await LoginPage.login('standard_user', 'secret_sauce');
         await InventoryPage.select('lohi');
 
-        all_prices_text = await InventoryPage.productPrices.map(function(element) {
-            return element.getText()
-        });
-
-        all_prices = all_prices_text.map(function(price) {
-            return parseFloat(price.replace('$', ''))
-        });
-
+        all_prices_text = await InventoryPage.productsPrices.map(element => element.getText());
+        all_prices = all_prices_text.map(price => parseFloat(price.replace('$', '')));
         all_prices_ordered = _.orderBy(all_prices, [], ['asc']);
 
         expectChai(_.isEqual(all_prices, all_prices_ordered)).to.equal(true, "Sort products by price ascending is not applied");
     })
 
 
-    it('Sort products by price (high to low) ', async () => {
+    it('Sort products by price (high to low)', async () => {
         await LoginPage.login('standard_user', 'secret_sauce');
         await InventoryPage.select('hilo');
 
-        all_prices_text = await InventoryPage.productPrices.map(function(element) {
-            return element.getText()
-        });
-
-        all_prices = all_prices_text.map(function(price) {
-            return parseFloat(price.replace('$', ''))
-        });
-
+        all_prices_text = await InventoryPage.productsPrices.map(element => element.getText());
+        all_prices = all_prices_text.map(price => parseFloat(price.replace('$', '')));
         all_prices_ordered = _.orderBy(all_prices, [], ['desc']);
 
         expectChai(_.isEqual(all_prices, all_prices_ordered)).to.equal(true, "Sort products by price descending is not applied");
     })
 
 
-    it('Sort products by name (a to z) ', async () => {
+    it('Sort products by name (a to z)', async () => {
         await LoginPage.login('standard_user', 'secret_sauce');
 
         await InventoryPage.select('az');
 
-        all_products = await InventoryPage.productTitles.map(function(element) {
-            return element.getText()
-        });
-
+        all_products = await InventoryPage.productsTitles.map(element => element.getText());
         all_products_ordered = _.orderBy(all_products, [], ['asc']);
         
         expectChai(_.isEqual(all_products, all_products_ordered)).to.equal(true, "Sort products by name ascending is not applied");
@@ -65,18 +53,39 @@ describe('Inventory Page', () => {
     })
 
 
-    it('Sort products by name (z to a) ', async () => {
+    it('Sort products by name (z to a)', async () => {
         await LoginPage.login('standard_user', 'secret_sauce');
 
         await InventoryPage.select('za');
 
-        all_products = await InventoryPage.productTitles.map(function(element) {
-            return element.getText()
-        });
-
+        all_products = await InventoryPage.productsTitles.map(element => element.getText());
         all_products_ordered = _.orderBy(all_products, [], ['desc']);
 
         expectChai(_.isEqual(all_products, all_products_ordered)).to.equal(true, "Sort products by name descending is not applied");
     })
+
+
+    // We can repeat this test for every element (name, description, price) in product component
+    it('Products images appearance for standard_user', async () => {
+
+        await LoginPage.login('standard_user', 'secret_sauce');
+
+        all_images = await InventoryPage.productsImages.map(element => element.getAttribute('src'));
+        all_images_expected = items.map(i => i.image);
+
+        expectChai(_.isEqual(all_images, all_images_expected)).to.equal(true, "Product images are not displayed correctly for standard_user");
+
+    });
+
+    it('Product images appearance for problem_user', async () => {
+
+        await LoginPage.login('problem_user', 'secret_sauce');
+       
+        all_images = await InventoryPage.productsImages.map(element => element.getAttribute('src'));
+        all_images_expected = items.map(i => i.image);
+        
+        expectChai(_.isEqual(all_images, all_images_expected)).to.equal(true, "Product images are not displayed correctly for problem_user");
+
+    });
 
 })
